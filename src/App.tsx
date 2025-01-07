6a5bad349b3589ea0c521f54cf4a316405308e1b
Noviiich <nowikoff63@gmail.com>
2025-01-07 22:16:36 +0300

    add two button: Deposit and Withdraw

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import WebApp from '@twa-dev/sdk'
 
-function App() {
-  const [count, setCount] = useState(0)
+WebApp.ready();
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
-        </button>
-      </div>
+      <div className="container">
+        <h1>Manage Your Tokens</h1>
+        <p className="info">Quickly deposit or withdraw your tokens.</p>
+        <div className="buttons">
+            <button className="button deposit">Deposit</button>
+            <button className="button withdraw">Withdraw</button>
+        </div>
+    </div>
     </>
   )
 }
