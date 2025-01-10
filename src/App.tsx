492de71ee3ccaefc1f9d5a7c1ef6089fc6500544
Noviiich <nowikoff63@gmail.com>
2025-01-10 20:35:12 +0300

    delete function handleDeposit

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,12 @@ import { Header } from './components/Header/Header';
 WebApp.ready();
 
 function App() {
-  const handleDeposit = () => {
-    const depositAmount = 1000000; // Сумма в нанокоинах (1 TON = 1e9 нанокоинов)
-    const walletUrl = `https://t.me/Wallet?start=deposit_ton&amount=${depositAmount}`;
-    window.open(walletUrl, '_blank'); // Открыть Telegram Wallet в новом окне
-  };
 
   return (
     <>
     <TonConnectUIProvider manifestUrl="https://<YOUR_APP_URL>/tonconnect-manifest.json">
     <div className="app">
       <Header />
-        {/* <h1>Manage Your Tokens</h1>
-        <p className="info">Quickly deposit or withdraw your tokens.</p>
-        <div className="buttons">
-            <button className="button deposit" onClick={handleDeposit}>Deposit</button>
-            <button className="button withdraw">Withdraw</button>
-        </div> */}
     </div>
     </TonConnectUIProvider>
     </>
