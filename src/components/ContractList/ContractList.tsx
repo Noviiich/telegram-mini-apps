8ec5d2772751909483051e4e1c73233560e8d6a7
Noviiich <nowikoff63@gmail.com>
2025-01-11 17:25:33 +0300

    choose path img

--- src/components/ContractList/ContractList.tsx
+++ src/components/ContractList/ContractList.tsx
@@ -3,9 +3,9 @@ import { Card } from "../Card/Card";
 
 export const ContractList: React.FC = () => {
     const contracts = [
-        { id: 1, name: "Contract #1", image: '/static/1.jpg' },
-        { id: 2, name: "Contract #2", image: "/static/2.jpg" },
-        { id: 3, name: "Contract #3", image: "/static/3.jpg" }
+        { id: 1, name: "Contract #1", image: 'https://noviiich.github.io/telegram-mini-apps/static/1.jpg' },
+        { id: 2, name: "Contract #2", image: "https://noviiich.github.io/telegram-mini-apps/static/2.jpg" },
+        { id: 3, name: "Contract #3", image: "https://noviiich.github.io/telegram-mini-apps/static/3.jpg" }
       ];
 
   const handleDeposit = (id: number) => {
