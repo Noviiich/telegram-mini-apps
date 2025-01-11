144537887f51559702da94f23f2bc08191464a72
Lapo4kaKek <Sheydos2003@yandex.ru>
2025-01-11 12:08:04 +0300

    feature(components): added CardList, ContractList and other updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import './App.css'
 import WebApp from '@twa-dev/sdk'
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import { Header } from './components/Header/Header';
+import { ContractList } from "./components/ContractList/ContractList";
 
 WebApp.ready();
 
 function App() {
-
   return (
-    <>
     <TonConnectUIProvider manifestUrl="https://noviiich.github.io/telegram-mini-apps/tonconnect-manifest.json">
-    <div className="app">
-      <Header />
-    </div>
+      <div className="app">
+        <h1>My App with React UI</h1>
+        <button className="connect">Connect Wallet</button>
+        <ContractList />
+      </div>
     </TonConnectUIProvider>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
