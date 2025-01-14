85a5385e7dc2659fe6a6fc0042c823a91da54fe8
Lapo4kaKek <Sheydos2003@yandex.ru>
2025-01-14 19:29:23 +0300

    some refactors

--- src/components/ModalToken/ModalToken.tsx
+++ src/components/ModalToken/ModalToken.tsx
@@ -13,8 +13,8 @@ export const ModalToken: React.FC<TokenProps> = ({ data, close }) => {
       <div className="overlay" onClick={close}></div>
       <div className="modal" onClick={(e) => e.stopPropagation()}>
           <img src={data?.image} alt={data?.name} />
-          <h2>Подробности Карточки {data?.id}</h2>
-          <p>Здесь будет подробная информация о  карточке.</p>
+          <h2>Some details {data?.id}</h2>
+          <p>Some information about token</p>
           <div>
             <button onClick={close}>Deposit</button>
             <button onClick={close}>Withdraw</button>
