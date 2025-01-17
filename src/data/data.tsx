b031d1b534428dbc5c76a99a58d43033ebf5a4ef
Noviiich <nowikoff63@gmail.com>
2025-01-17 18:48:23 +0300

    separated info about contract

---
+++ src/data/data.tsx
@@ -0,0 +1,19 @@
+export interface CardData {
+    id: number;
+    name: string;
+    image: string;
+  }
+  
+
+  export const contracts: CardData[] = [
+    { id: 1, name: "Contract #1", image: '/static/1.jpg' },
+    { id: 2, name: "Contract #2", image: "/static/2.jpg" },
+    { id: 3, name: "Contract #3", image: "/static/3.jpg" },
+    { id: 4, name: "Contract #4", image: '/static/1.jpg' },
+    { id: 5, name: "Contract #5", image: "/static/2.jpg" },
+    { id: 6, name: "Contract #6", image: "/static/3.jpg" },
+    { id: 7, name: "Contract #7", image: '/static/1.jpg' },
+    { id: 8, name: "Contract #8", image: "/static/2.jpg" },
+    { id: 9, name: "Contract #9", image: "/static/3.jpg" },
+    { id: 10, name: "Contract #10", image: "/static/3.jpg" }
+  ];
