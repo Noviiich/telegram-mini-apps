b947d078538a4c2fb3fcc997988c0a0c0f4b2f66
Noviiich <nowikoff63@gmail.com>
2025-01-17 18:47:00 +0300

    update logic in list

--- src/components/ContractList/ContractList.tsx
+++ src/components/ContractList/ContractList.tsx
@@ -1,23 +1,17 @@
 import React from "react";
 import { Card } from "../Card/Card";
 import "./ContractList.css";
-import { CardInfo } from "../../App";
+import { contracts } from "../../data/data";
 
-interface ContractListProps {
-  openToken: (card: CardInfo) => void;
-  data: Array<CardInfo>;
-}
-
-
-export const ContractList: React.FC<ContractListProps> = ({data, openToken}) => {
-
+export const ContractList: React.FC = ({}) => {
   return (
     <div className="contract-list">
-      {data.map((item) => (
+      {contracts.map((item) => (
         <Card
           key={item.id}
-          card={item}
-          open={openToken}
+          id={item.id}
+          name={item.name}
+          image={item.image}
         />
       ))}
     </div>
