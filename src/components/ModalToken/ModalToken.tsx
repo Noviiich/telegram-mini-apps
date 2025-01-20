cc4b636b48caebd6a036840ed149c5710a2d1a91
Noviiich <nowikoff63@gmail.com>
2025-01-20 18:44:57 +0300

    add new style in modal window

--- src/components/ModalToken/ModalToken.tsx
+++ src/components/ModalToken/ModalToken.tsx
@@ -5,19 +5,25 @@ import "./ModalToken.css";
 interface TokenProps {
   data: CardInfo | null;
   close: () => void;
+  position: number;
 }
 
-export const ModalToken: React.FC<TokenProps> = ({ data, close }) => {
+export const ModalToken: React.FC<TokenProps> = ({ data, close, position }) => {
   return (
     <>
       <div className="overlay" onClick={close}></div>
-      <div className="modal" onClick={(e) => e.stopPropagation()}>
+      <div className="modal" onClick={(e) => e.stopPropagation()} style={{ top: `${position}px` }}>
           <img src={data?.image} alt={data?.name} />
           <h2>Some details {data?.id}</h2>
-          <p>Some information about token</p>
-          <div>
-            <button onClick={close}>Deposit</button>
-            <button onClick={close}>Withdraw</button>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere
+          varius est, nec facilisis nisi eleifend quis. Ut tincidunt, turpis ut
+          venenatis dapibus, metus arcu feugiat elit, id consectetur justo magna
+          non metus.
+        </p>
+          <div className="buttons">
+            <button className="button-red" onClick={close}>Deposit</button>
+            <button className="button-green" onClick={close}>Withdraw</button>
           </div>
       </div>
     </>
